a5281d32b90f5b0ccb580f7fe76ff98e4d8a1dc2
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-09 22:44:43 +0600

    creating necessage page and setup package

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css'
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+import DashboardLayout from "./components/Dashboard/Layouts/DashboardLayout";
+import FrontPartLayout from "./components/FrontPart/Layouts/FrontPartLayout";
+import Home from "./components/FrontPart/Pages/home";
+import About from "./components/FrontPart/Pages/about";
+import NotFound from "./components/FrontPart/Pages/notFound";
+
 function App() {
- 
+  const location = useLocation();
+  const isDashboardPage = location.pathname.startsWith("/dashboard");
 
   return (
-    <div>
-      
-        
-    </div>
-  )
+    <>
+      {isDashboardPage ? (
+        <DashboardLayout>
+          <Routes></Routes>
+        </DashboardLayout>
+      ) : (
+        <FrontPartLayout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </FrontPartLayout>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Dashboard/Layouts/DashboardLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashboardLayout = ({children}) => {
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
