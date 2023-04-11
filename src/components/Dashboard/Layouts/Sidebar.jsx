d10364d42d8d1e8cbcf554a272aca6bf25eb8b9e
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-12 00:11:42 +0600

    -

--- src/components/Dashboard/Layouts/Sidebar.jsx
+++ src/components/Dashboard/Layouts/Sidebar.jsx
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 
 const Sidebar = () => {
   const [sideBarOpen, setSideBarOpen] = useState(true);
-  console.log(sideBarOpen)
+  console.log(sideBarOpen);
   return (
     <>
       <span
         class="absolute text-white text-4xl top-5 right-4 cursor-pointer"
-        onClick={()=> setSideBarOpen(!sideBarOpen)}
+        onClick={() => setSideBarOpen(!sideBarOpen)}
       >
         <i class="ri-menu-3-line px-2 bg-gray-900 rounded-md"></i>
       </span>
-      <div class={`sidebar fixed ${!sideBarOpen && "hidden"} top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900`}>
+      <div
+        class={`sidebar fixed ${
+          !sideBarOpen && "hidden"
+        } top-0 bottom-0 lg:left-0 p-2 ${!sideBarOpen && "-translate-x-full"} w-[300px] overflow-y-auto text-center bg-gray-900`}
+      >
         <div class="text-gray-100 text-xl">
           <div class="p-2.5 mt-1 flex items-center">
             <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
@@ -19,8 +23,8 @@ const Sidebar = () => {
               TailwindCSS
             </h1>
             <i
-              class="bi bi-x cursor-pointer ml-28 lg:hidden"
-              onClick={()=> setSideBarOpen(!sideBarOpen)}
+              class="ri-menu-3-line cursor-pointer ml-28 lg:hidden"
+              onClick={() => setSideBarOpen(!sideBarOpen)}
             ></i>
           </div>
           <div class="my-2 bg-gray-600 h-[1px]"></div>
