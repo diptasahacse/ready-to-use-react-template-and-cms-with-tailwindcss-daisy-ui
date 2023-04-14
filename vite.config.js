19e8aae9c3ae1383893bff064744b9165d542424
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-15 02:00:22 +0600

    -

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
   plugins: [react()],
 })
