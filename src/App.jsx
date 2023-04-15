812aa477d66146a3041e547ce8bceaf696ffc95f
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-15 23:59:40 +0600

    -

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,20 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 import "./App.css";
 import FrontPartLayout from "./components/FrontPart/Layouts/FrontPartLayout";
-
-
-import AuthLayout from "./components/FrontPart/Layouts/AuthLayout";
-
-import DashboardLayout from "./components/Dashboard/Layouts/DashboardLayout";
-
-
-
-
 import Home from "./components/FrontPart/Pages/Home";
 import Blogs from "./components/FrontPart/Pages/Blogs";
+import AuthLayout from "./components/FrontPart/Layouts/AuthLayout";
 import SignIn from "./components/FrontPart/Pages/SinIn";
 import Register from "./components/FrontPart/Pages/Register";
 import PasswordReset from "./components/FrontPart/Pages/PasswordReset";
+import DashboardLayout from "./components/Dashboard/Layouts/DashboardLayout";
 import Dashboard from "./components/Dashboard/Pages/Dashboard";
 import DashboardProfile from "./components/Dashboard/Pages/DashboardProfile";
 import DashboardProfileSettings from "./components/Dashboard/Pages/DashboardProfileSettings";
 import DashboardBlogs from "./components/Dashboard/Pages/DashboardBlogs";
 import AllUsers from "./components/Dashboard/Pages/AllUsers";
 import NotFound from "./components/FrontPart/Pages/NotFound";
+
 
 function App() {
   const router = createBrowserRouter([
